a596e445ec8b18f068aedadf1a8a2c30b1e8ef3e
HabibMollah <hrmollah2002@gmail.com>
2023-08-01 16:35:45 +0600

    style search bar

--- src/components/common/Navbar/SearchBar.tsx
+++ src/components/common/Navbar/SearchBar.tsx
@@ -1,12 +1,22 @@
 import { HiSearch } from 'react-icons/hi';
 export default function SearchBar() {
   return (
-    <div>
-      <input type="text" placeholder="Search by Network, Offers..." />
-      <button className="flex items-center">
-        <HiSearch />
-        Search
-      </button>
-    </div>
+    <form className="rounded-full bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] p-[3px]">
+      <div className="flex">
+        <button className="rounded-l-full bg-white  px-4">All</button>
+        <div className="relative w-full">
+          <input
+            className="z-20 block w-full rounded-r-full p-2.5 text-sm text-black"
+            placeholder="Search by Network, Offers..."
+            required
+          />
+          <button
+            type="submit"
+            className="absolute right-0 top-0 flex h-full items-center gap-2 rounded-r-full bg-blue-700 p-2.5 text-sm font-medium text-white">
+            <HiSearch /> Search
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -4,7 +4,7 @@ import MenuBar from './MenuBar';
 
 export default function NavBar() {
   return (
-    <nav className="h-[186px] outline">
+    <nav className="h-[186px]">
       <div className="flex items-center justify-evenly">
         <Image
           width={156}
@@ -12,10 +12,10 @@ export default function NavBar() {
           src="/affling-logo.svg"
           alt="Affling Logo"
         />
-        <div>
+        <div className="">
           <SearchBar />
         </div>
-        <button className="h-[63.27px] rounded-[20px] bg-blue-500 bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] px-[35px] py-[10px] text-center text-[26px] font-bold text-white">
+        <button className="h-[63.27px] rounded-[20px] bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] px-[35px] py-[10px] text-center text-[26px] font-bold text-white">
           Sign Up
         </button>
       </div>
