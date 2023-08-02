d01ad262a3b97bca21a8057ccd56508f6d00f450
HabibMollah <hrmollah2002@gmail.com>
2023-08-02 19:55:50 +0600

    finish faq page

--- src/components/common/SimpleBanner/index.tsx
+++ src/components/common/SimpleBanner/index.tsx
@@ -1,19 +1,29 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
   className?: string;
-  img:string
+  img: string;
+  description?: string;
+  subdescription?: string;
 }
 
-const SimpleBanner = ({ children, className,img }: Props) => {
+const SimpleBanner = ({
+  children,
+  className,
+  img,
+  description,
+  subdescription,
+}: Props) => {
   return (
     <div
-      className={` ${className} mb-[50px] h-[580px]  ${img} text-white    bg-cover bg-center   bg-no-repeat `}>
-      <div className="h-[580px] bg-[#4E93D3] opacity-80">
+      className={` ${className} mb-[50px] h-[580px]  ${img} bg-cover    bg-center bg-no-repeat   text-white `}>
+      <div className="h-[580px] bg-[#4E93D3] text-center opacity-80">
         <h2 className="pt-[170px] text-center text-5xl font-bold leading-[50px] text-white opacity-100">
           {children}
         </h2>
+        <p className="my-3 text-4xl font-bold">{description}</p>
+        <p className="font-bold">{subdescription}</p>
       </div>
     </div>
   );
