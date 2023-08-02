7f644633bfdd092a1f28743db20b80f62774a26d
tahamah <taha.iu.bd@gmail.com>
2023-08-02 21:37:47 +0600

    num done

--- src/components/common/typography/titles.tsx
+++ src/components/common/typography/titles.tsx
@@ -15,17 +15,30 @@ export const BlueTitle = ({ children, className }: BlueTitle) => {
 };
 
 export const BlueTitleNum = ({ children, className, num }: BlueTitleNum) => {
+  const commonClass = 'mr-7 rounded-full bg-[#4e93d3] font-bold text-white';
   return (
     <h3 className={`blue-title mb-5 ${className}`}>
-      <span
-        className={`mr-7 rounded-full bg-[#4e93d3]  ${
-          num === 1 ? 'px-3' : 'px-2.5'
-        } 
-        ${num < 10 ? 'py-0.5' : 'py-1.5'}
-        
-         font-bold text-white`}>
-        {num}
-      </span>
+      {num === 11 && (
+        <span
+          className={`   px-3 ${
+            num >= 10 && 'py-2.5 text-[18px]'
+          }  ${commonClass}`}>
+          {num}
+        </span>
+      )}
+      
+      {num === 1 && (
+        <span className={`px-3 py-0.5 ${commonClass}`}>{num}</span>
+      )}
+
+      {num > 1 && num < 10 && (
+        <span className={`px-3 py-1 ${commonClass}`}>{num}</span>
+      )}
+
+      {(num === 10 || num === 12) && (
+        <span className={`px-1 py-1 ${commonClass}`}>{num}</span>
+      )}
+
       {children}
     </h3>
   );
