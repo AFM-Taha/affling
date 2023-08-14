638609c27448728270dc241720d849eb25ca84a4
HabibMollah <hrmollah2002@gmail.com>
2023-08-15 00:54:47 +0600

    create layout for user and admin

---
+++ src/layout/UserLayout.tsx
@@ -0,0 +1,13 @@
+import Footer from '@/components/common/Footer';
+import Navbar from '@/components/common/Navbar';
+import { ReactNode } from 'react';
+
+export default function UserLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="bg-[#ffffff]">
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+}
