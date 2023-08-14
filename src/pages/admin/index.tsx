b98201a8eb7b5c2046e4542696a681ef205803d7
HabibMollah <hrmollah2002@gmail.com>
2023-08-15 01:15:42 +0600

    remove unused variables

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,3 +1,11 @@
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export default function Admin() {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    push('/admin/dashboard');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 }
