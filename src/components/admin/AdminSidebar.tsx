f68c992257a279c2810de4ab20db1717bef1c5d6
HabibMollah <hrmollah2002@gmail.com>
2023-08-15 11:39:08 +0600

    finish sidebar design

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import SidebarCollapsible from './SidebarCollapsible';
+import Link from 'next/link';
+import { MdOutlineHelpOutline } from 'react-icons/md';
 
 export default function AdminSidebar() {
   return (
-    <aside className="h-screen w-[285px] bg-[#171616] pt-14 text-white">
-      <div className="flex items-center justify-start gap-3 border-l-[5px] border-[#1AA5CF] bg-[#1aa5cf26] py-4 pl-4 text-xl text-[#1AA5CF]">
+    <aside className="relative h-screen w-[285px] bg-[#171616] pt-14 text-white">
+      <div className="mb-9 flex items-center justify-start gap-3 border-l-[5px] border-[#1AA5CF] bg-[#1aa5cf26] py-4 pl-4 text-xl text-[#1AA5CF]">
         <Image
           className="-mt-1"
           width={20}
@@ -11,7 +14,14 @@ export default function AdminSidebar() {
           src="/star-part-of-logo.svg"
           alt="Affling Logo"
         />
-        <h1>Dashboard</h1>
+        <h1 className="font-medium">Dashboard</h1>
+      </div>
+      <SidebarCollapsible />
+      <div className="absolute bottom-5 left-12 text-[#808291]">
+        <Link href="/help" className="flex items-center justify-center gap-4">
+          <MdOutlineHelpOutline size={24} color="#808291" />
+          <div className="text-lg">Help & Center</div>
+        </Link>
       </div>
     </aside>
   );
