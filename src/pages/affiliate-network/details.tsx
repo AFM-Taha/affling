0f741ed055b8af02133f976713e16e8786034392
HabibMollah <hrmollah2002@gmail.com>
2023-08-17 14:29:51 +0600

    moke a separate rating distribution component

--- src/pages/affiliate-network/details.tsx
+++ src/pages/affiliate-network/details.tsx
@@ -1,5 +1,6 @@
 import CoverPhoto from '@/components/common/Cover/CoverPhoto';
 import BigRatingStar from '@/components/common/Rating/BigRatingStar';
+import RatingDistribution from '@/components/common/Rating/RatingDistribution';
 import Table from '@/components/common/Table/Table';
 import { Progress } from '@/components/ui/progress';
 import Image from 'next/image';
@@ -74,49 +75,11 @@ export default function Details() {
             <Table />
           </div>
           {/* --- Rating Distribution ---  */}
-          <div className="ml-7 min-w-[424px]">
-            <h5 className="mb-6 text-lg font-bold">Rating Distribution</h5>
-            <div className="flex items-center justify-evenly gap-2">
-              <p className="">5 star</p>
-              <Progress
-                className="h-3 max-w-[326px] border border-[#4E93D3] bg-[#EBEDED]"
-                value={68}
-              />
-              <p className="">68%</p>
-            </div>
-            <div className="flex items-center justify-evenly gap-2">
-              <p className="">4 star</p>
-              <Progress
-                className="h-3 max-w-[326px] border border-[#4E93D3] bg-[#EBEDED]"
-                value={20}
-              />
-              <p className="">20%</p>
-            </div>
-            <div className="flex items-center justify-evenly gap-2">
-              <p className="">3 star</p>
-              <Progress
-                className="h-3 max-w-[326px] border border-[#4E93D3] bg-[#EBEDED]"
-                value={10}
-              />
-              <p className="">10%</p>
-            </div>
-            <div className="flex items-center justify-evenly gap-2">
-              <p className="">2 star</p>
-              <Progress
-                className="h-3 max-w-[326px] border border-[#4E93D3] bg-[#EBEDED]"
-                value={2}
-              />
-              <p className="">2%</p>
-            </div>
-            <div className="flex items-center justify-evenly gap-2">
-              <p className="">1 star</p>
-              <Progress
-                className="h-3 max-w-[326px] border border-[#4E93D3] bg-[#EBEDED]"
-                value={0}
-              />
-              <p className="">0%</p>
-            </div>
-          </div>
+          <RatingDistribution />
+        </div>
+        {/* --- All Reviews --- */}
+        <div>
+          <h5 className="text-lg font-bold">All Reviews</h5>
         </div>
       </div>
     </div>
