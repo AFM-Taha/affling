fc45e8cf60b9be827aa4b3673aa96334f030d923
HabibMollah <hrmollah2002@gmail.com>
2023-08-20 00:56:52 +0600

    improve sign up button responsiveness

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -5,14 +5,14 @@ import MenuBar from './MenuBar';
 export default function NavBar() {
   return (
     <nav className="h-[183px]">
-      <div className="my-3 flex items-center justify-between lg:pl-[87px]">
+      <div className="my-3 flex items-center justify-between gap-2 lg:pl-[87px]">
         <div className="relative ml-4 h-20 w-20 lg:h-[131px] lg:w-[156px]">
           <Image layout="fill" src="/affling-logo.svg" alt="Affling Logo" />
         </div>
         <div className="hidden w-full max-w-4xl sm:block">
           <SearchBar />
         </div>
-        <button className="rounded-[20px] bg-gradient-to-r  from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] font-bold  text-white">
+        <button className="mr-8 rounded-[20px] bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] px-4 py-2 font-bold text-white  lg:text-2xl">
           Sign Up
         </button>
       </div>
