656e4e121e14a122a9f107abcd8ed1be88248933
HabibMollah <hrmollah2002@gmail.com>
2023-08-20 00:46:31 +0600

    improve searchbar responsiveness

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -9,7 +9,7 @@ export default function NavBar() {
         <div className="relative ml-4 h-20 w-20 lg:h-[131px] lg:w-[156px]">
           <Image layout="fill" src="/affling-logo.svg" alt="Affling Logo" />
         </div>
-        <div className="w-full max-w-4xl">
+        <div className="hidden w-full max-w-4xl sm:block">
           <SearchBar />
         </div>
         <button className="rounded-[20px] bg-gradient-to-r  from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] font-bold  text-white">
@@ -18,6 +18,9 @@ export default function NavBar() {
       </div>
       <div className="w-full bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] py-2 font-bold text-white">
         <MenuBar />
+      </div>
+      <div className="w-full max-w-4xl py-1 sm:hidden">
+        <SearchBar />
       </div>
     </nav>
   );
