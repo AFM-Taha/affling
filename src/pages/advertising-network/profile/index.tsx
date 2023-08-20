5b1ce0c96dc8c851848b0f62652d55712069419d
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-08-20 21:50:56 +0600

    Type error fix

--- src/pages/advertising-network/profile/index.tsx
+++ src/pages/advertising-network/profile/index.tsx
@@ -1,12 +1,6 @@
-import CoverPhoto from '@/components/common/Cover/CoverPhoto';
-import BigRatingStar from '@/components/common/Rating/BigRatingStar';
-import RatingDistribution from '@/components/common/Rating/RatingDistribution';
-import Table from '@/components/common/Table/Table';
-import Image from 'next/image';
-import { BiSolidStar } from 'react-icons/bi';
-import { IoMdAddCircle } from 'react-icons/io';
+
 import useGet from '@/hooks/useGet';
-import Details from '@/pages/affiliate-network/details';
+
 
 interface FetchedNetworkDetails {
   name: string;
