1ba59d70f981d9b3f9829ee2bef2d734cdb4dccb
HabibMollah <hrmollah2002@gmail.com>
2023-08-22 02:01:10 +0600

    conditional rendering for number type

--- src/components/common/Forms/InputField.tsx
+++ src/components/common/Forms/InputField.tsx
@@ -8,6 +8,7 @@ const InputField: React.FC<IInputField> = ({
   type,
   register,
   errors,
+  ...rest
 }) => {
   return (
     <div>
@@ -16,13 +17,25 @@ const InputField: React.FC<IInputField> = ({
         className="mt-4 block text-base font-bold leading-relaxed text-zinc-800">
         {label}
       </label>
-      <input
-        id={id}
-        type={type}
-        placeholder={placeholder || undefined}
-        className="h-[37.07px] w-full bg-stone-100 pl-2"
-        {...register(id)}
-      />
+      {type === 'number' ? (
+        <input
+          {...rest}
+          id={id}
+          type={type}
+          placeholder={placeholder || undefined}
+          className="h-[37.07px] w-full bg-stone-100 pl-2"
+          {...register(id, { valueAsNumber: true })}
+        />
+      ) : (
+        <input
+          id={id}
+          type={type}
+          placeholder={placeholder || undefined}
+          className="h-[37.07px] w-full bg-stone-100 pl-2"
+          {...register(id)}
+        />
+      )}
+
       <p className="text-red-500">{errors[id] ? errors[id]?.message : ''}</p>
     </div>
   );
