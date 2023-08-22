299a90105dab69dfefb7d8366a07f1de5c5b0102
HabibMollah <hrmollah2002@gmail.com>
2023-08-23 02:01:00 +0600

    finish usePost hook

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -2,8 +2,8 @@ import baseURL from '@/assets/baseURL';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 
-export default function usePost(urlEndpoint: string) {
-  return useMutation({
-    mutationFn: (data) => axios.post(`${baseURL}/${urlEndpoint}`, data),
+export default function usePost<T>(urlEndpoint: string) {
+  return useMutation<unknown, Error, T>({
+    mutationFn: (data) => axios.post<T>(`${baseURL}/${urlEndpoint}`, data),
   });
 }
