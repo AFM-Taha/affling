4a3224d1a870ad5cac260ed8723ed1f68143f244
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-08-22 22:58:57 +0600

    homepage

--- src/layout/UserLayout.tsx
+++ src/layout/UserLayout.tsx
@@ -1,5 +1,5 @@
-import Footer from '@/components/common/Footer';
-import Navbar from '@/components/common/Navbar';
+// import Footer from '@/components/common/Footer';
+// import Navbar from '@/components/common/Navbar';
 import { ReactNode } from 'react';
 
 export default function UserLayout({ children }: { children: ReactNode }) {
