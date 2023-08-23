4f6f718c574d1298f6de45278e760f97ae1958d4
HabibMollah <hrmollah2002@gmail.com>
2023-08-23 16:48:09 +0600

    create affiliate network sign up page

---
+++ src/pages/affiliate-network/index.tsx
@@ -0,0 +1,17 @@
+import SignUpForm from '@/components/SignUpForm/SignUpFormAdvertisingNetwork';
+import SignUpPageHero from '@/components/common/hero/SignUpPageHero';
+
+export default function AffiliateNetwork() {
+  return (
+    <div className="mx-auto min-h-screen px-5 lg:px-[140px]">
+      {/* advertising-network add section  */}
+      <div>
+        <SignUpPageHero />
+      </div>
+      {/* signUp From  */}
+      <div className="mr-auto mt-12 max-w-screen-md">
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
