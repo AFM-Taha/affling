c4d0be785ccc0c7499b058ef42560b1843b231c5
HabibMollah <hrmollah2002@gmail.com>
2023-08-23 18:07:28 +0600

    add tracking software page

---
+++ src/pages/tracking-software/index.tsx
@@ -0,0 +1,17 @@
+import SignUpForm from '@/components/SignUpForm/SignUpForm';
+import SignUpPageHero from '@/components/common/hero/SignUpPageHero';
+
+export default function TrackingSoftware() {
+  return (
+    <div className="mx-auto min-h-screen px-5 lg:px-[140px]">
+      {/* advertising-network add section  */}
+      <div>
+        <SignUpPageHero />
+      </div>
+      {/* signUp From  */}
+      <div className="mr-auto mt-12 max-w-screen-md">
+        <SignUpForm programType="Tracking Software" />
+      </div>
+    </div>
+  );
+}
