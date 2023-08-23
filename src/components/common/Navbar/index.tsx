ef4912d61718ac17fde9fa06a28e9e0d149f44f5
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-08-23 21:31:21 +0600

    header and footer done

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,27 +1,63 @@
 import Image from 'next/image';
-import SearchBar from './SearchBar';
+import { useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
 import MenuBar from './MenuBar';
+import SearchBar from './SearchBar';
+import MobileMenu from './MobileMenu';
+import Link from 'next/link';
 
 export default function NavBar() {
+  const [open, setToggle] = useState(false);
   return (
-    <nav className="h-[183px]">
-      <div className="my-3 flex items-center justify-between gap-2 lg:pl-[87px]">
-        <div className="relative ml-4 h-20 w-20 lg:h-[131px] lg:w-[156px]">
-          <Image layout="fill" src="/affling-logo.svg" alt="Affling Logo" />
+    <>
+      <nav className="hidden h-[183px] lg:block">
+        <div className="my-3  flex items-center justify-between gap-2 lg:pl-[87px]">
+          <div className="relative ml-4 h-20 w-12 lg:h-[131px] lg:w-[116px]">
+            <Link href="/">
+              <Image layout="fill" src="/affling-logo.svg" alt="Affling Logo" />
+            </Link>
+          </div>
+          <div className="hidden w-full max-w-4xl sm:block">
+            <SearchBar />
+          </div>
+          <button className="mr-8 rounded-[20px] bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] px-4 py-2 font-bold text-white  lg:text-2xl">
+            Sign Up
+          </button>
         </div>
-        <div className="hidden w-full max-w-4xl sm:block">
+        <div className="bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] py-2 font-bold text-white">
+          <MenuBar />
+        </div>
+        <div className="w-full max-w-4xl py-1 sm:hidden">
           <SearchBar />
         </div>
-        <button className="mr-8 rounded-[20px] bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] px-4 py-2 font-bold text-white  lg:text-2xl">
-          Sign Up
-        </button>
+      </nav>
+
+      {/* For mobile  menu*/}
+      <div className="mx-2 my-3 lg:hidden">
+        <div className="flex items-center justify-between">
+          <div className="flex space-x-1">
+            <AiOutlineMenu
+              className="cursor-pointer pt-3 text-4xl"
+              onClick={() => setToggle(true)}
+            />
+
+            <Image
+              width={55}
+              height={45}
+              src="/affling-logo.svg"
+              alt="Affling Logo"
+            />
+          </div>
+          <button className="mr-3 rounded-[20px] bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] px-4 py-2 font-bold text-white  lg:text-2xl">
+            Sign Up
+          </button>
+        </div>
+        <div className="mt-5">
+          <SearchBar />
+        </div>
       </div>
-      <div className="bg-gradient-to-r from-[#217CD9] via-[#218fd9e8] to-[#21C1D9] py-2 font-bold text-white">
-        <MenuBar />
-      </div>
-      <div className="w-full max-w-4xl py-1 sm:hidden">
-        <SearchBar />
-      </div>
-    </nav>
+
+      <MobileMenu open={open} setToggle={setToggle} />
+    </>
   );
 }
