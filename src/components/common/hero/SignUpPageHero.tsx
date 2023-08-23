755b7d6f5673813536dd9053029a15c1c79aa650
HabibMollah <hrmollah2002@gmail.com>
2023-08-24 04:19:46 +0600

    improve responsiveness

--- src/components/common/hero/SignUpPageHero.tsx
+++ src/components/common/hero/SignUpPageHero.tsx
@@ -1,18 +1,16 @@
 import Image from 'next/image';
-import signUpPageHeroPhoto  from '../../../assets/temp/SIgnUpPageHeroPhoto/SignUpPageHero.png'
+import signUpPageHeroPhoto from '../../../assets/temp/SIgnUpPageHeroPhoto/SignUpPageHero.png';
 import Link from 'next/link';
 const SignUpPageHero = () => {
   return (
     <div className="grid grid-cols-1 gap-5 pt-10 lg:grid-cols-3 lg:pt-5">
       {/* left side  */}
-      <div className=" h-44 border bg-zinc-300"></div>
+      <div className="col-span-1 h-full min-h-[200px] w-full border bg-zinc-300"></div>
       {/* right side  */}
-      <Link href="/" className=" h-44 border  lg:col-span-2">
+      <Link href="#" className="col-span-2 mx-auto border">
         <Image
           src={signUpPageHeroPhoto}
-          width={0}
-          height={0}
-          className='w-full h-full'
+          className="xl:h-[328.24px] xl:w-[872.96px]"
           alt=""
         />
       </Link>
