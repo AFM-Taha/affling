ea196a5a7596a8e49d74283768970e3664a6c098
HabibMollah <hrmollah2002@gmail.com>
2023-08-26 04:34:23 +0600

    add admin pages

---
+++ src/pages/admin/affiliate-network.tsx
@@ -0,0 +1,3 @@
+export default function AffiliateNetworkAdmin() {
+  return <div>AffiliateNetworkAdmin</div>;
+}
---
+++ src/pages/admin/affiliate-program.tsx
@@ -0,0 +1,3 @@
+export default function AffiliateProgramAdmin() {
+  return <div>AffiliateProgramAdmin</div>;
+}
