971c7a1bc504131886db0116dce5f2bbe05395b0
HabibMollah <hrmollah2002@gmail.com>
2023-09-04 22:28:52 +0600

    add toast on form submission

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <Layout>
         <Component {...pageProps} />
+        <Toaster />
       </Layout>
       <ReactQueryDevtools />
     </QueryClientProvider>
