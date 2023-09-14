4f308bfd196f6a525deb709bc40da46a0d5a0b19
HabibMollah <habib.prodev@gmail.com>
2023-09-15 02:43:09 +0600

    redirect to login with no auth

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,11 +1,14 @@
+import useToken from '@/hooks/useToken';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 export default function Admin() {
   const { push } = useRouter();
+  const token = useToken();
 
   useEffect(() => {
-    push('/admin/dashboard');
+    if (token) push('/admin/dashboard');
+    push('/admin/login');
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 }
