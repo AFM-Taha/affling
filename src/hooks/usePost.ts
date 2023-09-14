408428e0a9b5254c59aa3879dae9a6fe486cd905
HabibMollah <habib.prodev@gmail.com>
2023-09-15 02:01:54 +0600

    store token in cookie

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
 export default function usePost<T>(urlEndpoint: string) {
-  return useMutation<unknown, any, T>({
+  return useMutation<any, any, T>({
     mutationFn: (data) => axios.post<T>(`${baseURL}/${urlEndpoint}`, data),
     onSuccess: () => toast.success('Successfully registered!'),
     onError: (error) => {
