3a9dd8fd2969d9b571eb79c9526a9566341d43ed
HabibMollah <habib.prodev@gmail.com>
2023-09-15 09:37:17 +0600

    improve responsiveness and add logout button

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -5,29 +5,45 @@ import { MdOutlineHelpOutline } from 'react-icons/md';
 import { useState } from 'react';
 import { HiMenu } from 'react-icons/hi';
 import AdminMenu from './AdminMenu';
+import { TbLogout2 } from 'react-icons/tb';
+import { destroyCookie } from 'nookies';
 
 export default function AdminSidebar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleLogOut = () => {
+    destroyCookie(null, 'fromClient');
+  };
+
   return (
     <>
-      <aside className="relative hidden  w-[285px] border-x border-x-[#3f4e69] bg-[#2A3447] pt-14 text-white lg:block">
-        <Link href={'/admin/dashboard'}>
-          <div className="mb-9 flex items-center justify-start gap-3 border-l-[5px] border-[#1AA5CF] bg-[#1aa5cf26] py-4 pl-4 text-xl text-[#1AA5CF]">
-            <Image
-              style={{ width: 'auto' }}
-              className="-mt-1"
-              width={20}
-              height={20}
-              src="/star-part-of-logo.svg"
-              alt="Affling Logo"
-            />
-            <h1 className="font-medium">Dashboard</h1>
-          </div>
-        </Link>
-        <SidebarCollapsible />
-        <div className="absolute bottom-5 left-12 text-[#808291]">
-          <Link href="/help" className="flex items-center justify-center gap-4">
+      <aside className="relative hidden h-[100dvh] w-[285px] border-x border-x-[#3f4e69] bg-[#2A3447] pb-4 pt-14 text-white lg:flex lg:flex-col lg:justify-between">
+        <div>
+          <Link href={'/admin/dashboard'}>
+            <div className="mb-9 flex items-center justify-start gap-3 border-l-[5px] border-[#1AA5CF] bg-[#1aa5cf26] py-4 pl-4 text-xl text-[#1AA5CF]">
+              <Image
+                style={{ width: 'auto' }}
+                className="-mt-1"
+                width={20}
+                height={20}
+                src="/star-part-of-logo.svg"
+                alt="Affling Logo"
+              />
+              <h1 className="font-medium">Dashboard</h1>
+            </div>
+          </Link>
+          <SidebarCollapsible />
+        </div>
+        <div className="flex flex-col items-center gap-3">
+          <button
+            onClick={handleLogOut}
+            className="flex w-11/12 items-center justify-center gap-2 rounded-lg bg-red-500 py-1 font-medium text-white hover:bg-red-600">
+            <TbLogout2 />
+            Log out
+          </button>
+          <Link
+            href="/help"
+            className="flex items-center justify-center gap-4 text-[#808291]">
             <MdOutlineHelpOutline size={24} color="#808291" />
             <div className="text-lg">Help & Center</div>
           </Link>
@@ -36,7 +52,7 @@ export default function AdminSidebar() {
 
       {/* Hamburger Menu */}
       <div className="lg:hidden">
-        <div className="flex justify-between px-3 items-center text-white">
+        <div className="flex items-center justify-between px-3 text-white">
           <button
             onClick={() => setIsOpen(true)}
             className="border-1 m-3 block rounded-md border border-[#576c93] p-1">
