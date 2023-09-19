d80a6ccebb1f581d6bc57cf37d077eb07fd3de86
HabibMollah <habib.prodev@gmail.com>
2023-09-19 15:26:30 +0600

    add spinner component

---
+++ src/components/ui/spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div
+      className="mx-auto my-14 h-12 w-12
+  animate-spin rounded-full border-8 border-solid border-blue-500 border-t-transparent"></div>
+  );
+}
