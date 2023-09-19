2bdac75fd837d1f03ac7c5de52d5806a32d3f17a
HabibMollah <habib.prodev@gmail.com>
2023-09-19 18:10:42 +0600

    make tracking software review dynamic

--- src/pages/tracking-software/[id]/review.tsx
+++ src/pages/tracking-software/[id]/review.tsx
@@ -1,31 +1,72 @@
 import ImageUploader from '@/components/common/Forms/ImageUploader';
 import RatingStars from '@/components/common/Rating/RatingStars';
+import useGet from '@/hooks/useGet';
+import usePut from '@/hooks/usePut';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
+import { FetchedTrackingSoftware } from './profile';
 
-export interface AffiliateProgramReviewFormData {
+export interface TrackingSoftwareReviewFormData {
   name: string;
   email: string;
+  // image: any;
   review: string;
   payment: number;
-  user_interface: number;
-  startup_commission: number;
+  features: number;
+  fraud_detection: number;
+  technology: number;
   customer_support: number;
   recommend: number;
 }
 
 export default function Review() {
-  const { register, handleSubmit, setValue } =
-    useForm<AffiliateProgramReviewFormData>();
+  const { query } = useRouter();
 
-  const onSubmit = (data: AffiliateProgramReviewFormData) => {
-    console.log(data);
+  const { data: fetchedData } = useGet<FetchedTrackingSoftware>(
+    `top-it/${query.id}`
+  );
+
+  const {
+    mutate,
+    isLoading,
+    isError,
+    data: response,
+  } = usePut(`top-it/review/${query.id}`);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<TrackingSoftwareReviewFormData>();
+
+  const onSubmit = async (data: TrackingSoftwareReviewFormData) => {
+    const formData = new FormData();
+    formData.append('name', data.name);
+    formData.append('email', data.email);
+    formData.append('review', data.review);
+    formData.append('payment', String(data.payment));
+    formData.append('features', String(data.features));
+    formData.append('fraud_detection', String(data.fraud_detection));
+    formData.append('technology', String(data.technology));
+    formData.append('customer_support', String(data.customer_support));
+    formData.append('recommend', String(data.recommend));
+    // formData.append('image', data.image[0]);
+
+    console.log(formData, 'formData');
+
+    mutate(formData);
+
+    console.log(response);
   };
 
   // Define a function to update the ratings in the form data
   const handleRatingChange = (name: string, rating: number) => {
-    setValue(name as keyof AffiliateProgramReviewFormData, rating); // Update the rating in the form data
+    setValue(name as keyof TrackingSoftwareReviewFormData, rating); // Update the rating in the form data
   };
+
+  console.error(errors);
 
   return (
     <div className="max-w-[648px] pl-[49px] pt-[49px]">
@@ -37,7 +78,7 @@ export default function Review() {
             height={42}
             width={42}
           />
-          <h3 className="text-[26px] font-bold">TopClientOffer</h3>
+          <h3 className="text-[26px] font-bold">{fetchedData?.data.title}</h3>
         </div>
         <div>
           <h3 className="mb-5 text-[26px] font-bold">Overall Rating</h3>
@@ -47,25 +88,35 @@ export default function Review() {
               <RatingStars onRatingChange={handleRatingChange} name="payment" />
             </div>
             <div className="mb-[10px] flex justify-between">
-              <div className="text-lg font-bold text-[#313131]">
-                User Interface
-              </div>
+              <div className="text-lg font-bold text-[#313131]">Features</div>
               <RatingStars
                 onRatingChange={handleRatingChange}
-                name="user_interface"
+                name="features"
               />
             </div>
             <div className="mb-[10px] flex justify-between">
-              <div className="text-lg font-bold text-[#313131]">Offer</div>
-              <RatingStars onRatingChange={handleRatingChange} name="offer" />
-            </div>
-            <div className="mb-[10px] flex justify-between">
               <div className="text-lg font-bold text-[#313131]">
-                Affiliate Support
+                Fraud Detection
               </div>
               <RatingStars
                 onRatingChange={handleRatingChange}
-                name="affiliate_support"
+                name="fraud_detection"
+              />
+            </div>
+            <div className="mb-[10px] flex justify-between">
+              <div className="text-lg font-bold text-[#313131]">Technology</div>
+              <RatingStars
+                onRatingChange={handleRatingChange}
+                name="technology"
+              />
+            </div>
+            <div className="mb-[10px] flex justify-between">
+              <div className="text-lg font-bold text-[#313131]">
+                Customer Support
+              </div>
+              <RatingStars
+                onRatingChange={handleRatingChange}
+                name="customer_support"
               />
             </div>
             <div className="mb-[10px] flex justify-between">
@@ -96,10 +147,10 @@ export default function Review() {
           type="text"
           id="email"
         />
-        <h3 className="mb-2 mt-10 text-[26px] font-bold">
+        {/* <h3 className="mb-2 mt-10 text-[26px] font-bold">
           Upload Your Affiliate Payment Screenshot
-        </h3>
-        <ImageUploader />
+        </h3> */}
+        {/* <ImageUploader register={register} /> */}
         <div className="mt-4">
           <label className="text-xl" htmlFor="review">
             Your Review
@@ -109,8 +160,10 @@ export default function Review() {
             className="min-h-[200px] w-full rounded-lg bg-[#D9D9D9] px-4 py-3"
             id="review"></textarea>
         </div>
+        {isError && <p className="text-red-500">Failed!</p>}
         <button
-          className="my-7 rounded-lg bg-[#4E93D3] px-7 py-2 text-xl text-white hover:bg-blue-600"
+          disabled={isLoading}
+          className="my-7 rounded-lg bg-[#4E93D3] px-7 py-2 text-xl text-white hover:bg-blue-600 disabled:opacity-30"
           type="submit">
           Submit
         </button>
