dd4c967fdb0c6293614c2bc90829d30c8ed3057a
HabibMollah <habib.prodev@gmail.com>
2023-09-19 14:52:26 +0600

    finish affiliate network review form

--- src/pages/affiliate-network/[id]/review.tsx
+++ src/pages/affiliate-network/[id]/review.tsx
@@ -1,11 +1,14 @@
 import ImageUploader from '@/components/common/Forms/ImageUploader';
 import RatingStars from '@/components/common/Rating/RatingStars';
+import usePut from '@/hooks/usePut';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 
 export interface AffiliateNetworkReviewFormData {
   name: string;
   email: string;
+  image: any;
   review: string;
   payment: number;
   user_interface: number;
@@ -15,17 +18,57 @@ export interface AffiliateNetworkReviewFormData {
 }
 
 export default function Review() {
-  const { register, handleSubmit, setValue } =
-    useForm<AffiliateNetworkReviewFormData>();
+  const { query } = useRouter();
 
-  const onSubmit = (data: AffiliateNetworkReviewFormData) => {
-    console.log(data);
+  const {
+    mutate,
+    isLoading,
+    isError,
+    data: response,
+  } = usePut(`top-it/review/${query.id}`);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<AffiliateNetworkReviewFormData>();
+
+  const onSubmit = async (data: AffiliateNetworkReviewFormData) => {
+    const formData = new FormData();
+    formData.append('name', data.name);
+    formData.append('email', data.email);
+    formData.append('review', data.review);
+    formData.append('payment', String(data.payment));
+    formData.append('user_interface', String(data.user_interface));
+    formData.append('offer', String(data.offer));
+    formData.append('affiliate_support', String(data.affiliate_support));
+    formData.append('recommend', String(data.recommend));
+    formData.append('image', data.image[0]); // Use data.image[0] because it's an array
+
+    console.log(formData, 'formData');
+
+    mutate(formData);
+    // const response = await axios.put(
+    //   `${baseURL}/top-it/review/${query.id}`,
+    //   formData,
+    //   {
+    //     headers: {
+    //       'Content-Type': 'multipart/form-data', // Important for file uploads
+    //     },
+    //   }
+    // );
+
+    // Handle the API response
+    console.log(response);
   };
 
   // Define a function to update the ratings in the form data
   const handleRatingChange = (name: string, rating: number) => {
     setValue(name as keyof AffiliateNetworkReviewFormData, rating); // Update the rating in the form data
   };
+
+  console.error(errors);
 
   return (
     <div className="max-w-[648px] pl-[49px] pt-[49px]">
@@ -99,7 +142,7 @@ export default function Review() {
         <h3 className="mb-2 mt-10 text-[26px] font-bold">
           Upload Your Affiliate Payment Screenshot
         </h3>
-        <ImageUploader />
+        <ImageUploader register={register} />
         <div className="mt-4">
           <label className="text-xl" htmlFor="review">
             Your Review
@@ -109,8 +152,10 @@ export default function Review() {
             className="min-h-[200px] w-full rounded-lg bg-[#D9D9D9] px-4 py-3"
             id="review"></textarea>
         </div>
+        {isError && <p className="text-red-500">Failed!</p>}
         <button
-          className="my-7 rounded-lg bg-[#4E93D3] px-7 py-2 text-xl text-white hover:bg-blue-600"
+          disabled={isLoading}
+          className="my-7 rounded-lg bg-[#4E93D3] px-7 py-2 text-xl text-white hover:bg-blue-600 disabled:opacity-30"
           type="submit">
           Submit
         </button>
