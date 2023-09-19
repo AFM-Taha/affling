d17402283d2f042a6465fc55c81d1d0325b925a0
HabibMollah <habib.prodev@gmail.com>
2023-09-19 16:37:30 +0600

    make marketing spy tools profile dynamic

--- src/pages/marketing-spy-tools/[id]/profile.tsx
+++ src/pages/marketing-spy-tools/[id]/profile.tsx
@@ -8,40 +8,61 @@ import BigRatingStar from '@/components/common/Rating/BigRatingStar';
 import RatingDistribution from '@/components/common/Rating/RatingDistribution';
 import TableItems from '@/components/companyProfile/TableItems';
 import { HiMail } from 'react-icons/hi';
-import TargetingOptimization from '@/components/companyProfile/TargetingOptimization';
 import ReviewCard from '@/components/companyProfile/ReviewCard';
+import { useRouter } from 'next/router';
+import Spinner from '@/components/ui/spinner';
 
-interface FetchedNetworkDetails {
-  name: string;
+export interface FetchedMarketingSpyTools {
+  success: boolean;
+  data: Data;
+}
+
+export interface Data {
+  status: string;
+  _id: string;
+  title: string;
+  company_email: string;
+  skype: string;
+  program_type:
+    | 'Tracking Software'
+    | 'Marketing Spy Tools'
+    | 'Affiliate Program'
+    | 'Advertising Network'
+    | 'Affiliate Network';
+  network_url: string;
+  social_page: string;
+  network_description: string;
+  minimum_payment: number;
+  payment_frequency: string;
+  payment_method: string;
+  referral_commission: number;
+  question_aria: string;
+  tag: string;
+  targeting_optimization: any[];
+  publishers_contact: any[];
+  features: string;
+  unlimited_offers: string;
+  setup_fees: string;
+  startup_pricing: number;
+  affiliate_advertiser_contacts: any[];
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  reviews: any[];
 }
 
 function AffiliateProgramProfile() {
-  const { data } = useGet<FetchedNetworkDetails>(
-    'top-it?filter=Advertising%20Network'
+  const { query } = useRouter();
+  const { data, isLoading, isError, error } = useGet<FetchedMarketingSpyTools>(
+    `top-it/${query.id}`
   );
 
-  console.log(data);
+  console.log(data, 'fetchedData');
 
-  const affiliate_advertiser_contacts = [
-    {
-      name: 'Alex Johnson',
-      contact: 'alex@advertisepro.com',
-    },
-    {
-      name: 'Max-well',
-      contact: 'max@advertisepro.com',
-    },
-  ];
-  const advertiser_commission_contacts = [
-    {
-      name: 'Olha TopClientOffer',
-      contact: 'alex@advertisepro.com',
-    },
-    {
-      name: 'Yudina TopClientOffer',
-      contact: 'max@advertisepro.com',
-    },
-  ];
+  if (isLoading) return <Spinner />;
+
+  if (isError)
+    return <p className="text-center text-3xl text-red-500">{error.message}</p>;
 
   return (
     <div className=" mx-auto min-h-screen px-5 lg:px-[140px]">
@@ -60,9 +81,9 @@ function AffiliateProgramProfile() {
           />
 
           <ProfileInfo
-            domain="www.adplexity.com"
-            name="Adplexity"
-            type="Marketing Spy Tools"
+            domain={data.data.network_url}
+            name={data.data.title}
+            type={data.data.program_type}
           />
         </div>
       </div>
@@ -70,33 +91,30 @@ function AffiliateProgramProfile() {
       {/* 2nd part */}
       <div className="mt-4 grid  grid-cols-1 gap-x-20 gap-y-20 lg:mt-8 lg:grid-cols-2">
         <div className="space-y-12">
-          <Description />
+          <Description text={data.data.network_description} />
           {/* For Affiliate */}
           <div className="">
-            <h5 className="mb-8 text-lg font-bold">For Affiliate</h5>
+            <h5 className="mb-8 text-lg font-bold">Spy Tools Information</h5>
             <div className="max-w-[872px]">
-              <TableItems item="Payment Option" value="$100/$500" />
+              <TableItems item="Pricing" value={data.data.startup_pricing} />
               <TableItems
-                item="Payment Cycle"
-                value=" Daily, Weekly, Bi-Weekly, Net-15, Net-30 "
+                item="Subscription Plan"
+                value={data.data.payment_frequency}
               />
               <TableItems
                 item="Payment Method"
-                value=" Paypal, Payoneer, Bitcoin, WebMoney Bank Wire "
+                value={data.data.payment_method}
               />
+              <TableItems item="Categories" value={data.data.features} />
               <TableItems
-                item="Comission System"
-                value="CPA, CPL, CPI, CPS, ResShare, SmartLink, more"
+                item="Referral Commission "
+                value={data.data.referral_commission}
               />
-              <TableItems item="Referral Commission " value="5%,2%,10%" />
-              <TableItems
-                item="Tracking Software"
-                value="Affigate ,HasOffer, OfferLook, Tune,Everflow, OfferIT"
-              />
+
               <div className="mb-5 flex items-center gap-x-3 bg-neutral-200 text-sm font-normal leading-relaxed text-black">
-                <p className="w-72">Affiliate Support Managers</p>
+                <p className="w-72">Customer Manager</p>
                 <div>
-                  {affiliate_advertiser_contacts.map((item) => {
+                  {data.data.publishers_contact.map((item) => {
                     return (
                       <div key={item?.name} className="flex  justify-between">
                         <p>{item?.name}</p>
@@ -125,93 +143,6 @@ function AffiliateProgramProfile() {
           </div>
 
           {/* For Affiliate end  */}
-
-          {/* for advertise Start */}
-
-          <div className="">
-            <h5 className="mb-8 text-lg font-bold">For Advertiser</h5>
-            <div className="max-w-[872px]">
-              <TableItems
-                item="Ad Categories"
-                value="Push, Popunder, Native, Banner, Display, Interstitial, In-Page Push, Video, more"
-              />
-              <TableItems
-                item="Cost of Model"
-                value="CPM, CPC, Smart bidding models "
-              />
-              <TableItems
-                item="Payment Method"
-                value=" Paypal, Payoneer, Bitcoin, WebMoney Bank Wire "
-              />
-              <TableItems
-                item="Payment Cycle"
-                value=" Daily, Weekly, Bi-Weekly, Net-15, Net-30 "
-              />
-              <TableItems
-                item="Payment Method"
-                value=" Paypal, Payoneer, Bitcoin, WebMoney Bank Wire "
-              />
-              <TableItems
-                item="Minimum Deposit"
-                value="$50, $100, $500, $1000"
-              />
-              <TableItems
-                item="Commission System"
-                value="CPA, CPL, CPI, CPS, ResShare, SmartLink, more"
-              />
-              <TableItems item="Referral Commission " value="5%, 2%, 10%" />
-              <TableItems
-                item="Daily Traffic Impression "
-                value="1billion, 2billion, 3billion, 4billion, 5billion"
-              />
-              <TableItems item="Top Country" value="US, CA, AU, UK,DE, RU" />
-              <TableItems
-                item="Top Verticals"
-                value="Adult, Gaming,Nutra, E-commerce, Dating, Sweeptakes, more"
-              />
-              <div className="mb-5 flex items-center gap-x-3 bg-neutral-200 text-sm font-normal leading-relaxed text-black">
-                <p className="w-72">Advertiser Communication Managers </p>
-                <div>
-                  {advertiser_commission_contacts.map((item) => {
-                    return (
-                      <div key={item?.name} className="flex  justify-between">
-                        <p>{item?.name}</p>
-                        <div className="flex items-center gap-2">
-                          <a href="https://www.skype.com" target="_blank">
-                            <RiSkypeFill size={24} className="text-sky-500" />
-                          </a>
-                          <a href={item?.contact} target="_blank">
-                            {' '}
-                            <HiMail size={24} className="text-[#4cc6ff]" />
-                          </a>
-                          <a href="https://www.telegram.com" target="_blank">
-                            {' '}
-                            <RiTelegramFill
-                              size={24}
-                              className="text-[#34AADF]"
-                            />
-                          </a>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* for advertise End */}
-
-          {/* Targeting & Optimization start  */}
-          <div>
-            <h5 className="mb-8 text-lg font-bold">Targeting & Optimization</h5>
-
-            <div>
-              <TargetingOptimization />
-            </div>
-          </div>
-
-          {/* Targeting & Optimization End  */}
         </div>
         {/* rating  */}
         <div className="">
