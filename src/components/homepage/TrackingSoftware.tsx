db1999990062e0f807a7ca5c69dbfa9018bad3cd
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-09-19 23:36:28 +0600

    fix link issue to push method

--- src/components/homepage/TrackingSoftware.tsx
+++ src/components/homepage/TrackingSoftware.tsx
@@ -3,14 +3,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { AiFillStar } from 'react-icons/ai';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const TrackingSoftware = ({ softwareData }: any) => {
   // console.log('softwareData', softwareData);
   const [visibleItems, setVisibleItems] = useState(4);
 
+  const router = useRouter();
+
   const handleSeeMoreClick = () => {
     setVisibleItems(visibleItems + 4);
   };
+
   return (
     <div>
       <div className="customShadow">
@@ -20,10 +24,12 @@ const TrackingSoftware = ({ softwareData }: any) => {
 
         <div>
           {softwareData?.slice(0, visibleItems).map((el: any, index: any) => (
-            <Link
+            <div
               key={index}
-              href={`/tracking-software/${el._id}/profile`}
-              className=" flex flex-col justify-between border-b px-1 py-5 hover:bg-black/10 md:flex-row lg:px-3">
+              onClick={() =>
+                router.push(`/tracking-software/${el._id}/profile`)
+              }
+              className=" flex cursor-pointer flex-col justify-between border-b px-1 py-5 hover:bg-black/10 md:flex-row lg:px-3">
               <div className="flex flex-col space-x-6 md:basis-11/12 md:flex-row">
                 <div className="pl-5 lg:basis-2/12 lg:pl-0">
                   <Image
@@ -36,23 +42,6 @@ const TrackingSoftware = ({ softwareData }: any) => {
                   />
                 </div>
                 <div className="lg:basis-11/12">
-                  <h2 className="font-bold capitalize">{el.title}</h2>
-                  <p>{el.program_type}</p>
-                  <div className="flex items-center space-x-8">
-                    <div className="flex ">
-                      <AiFillStar className="text-[#FFAB2D]" />
-                      <AiFillStar className="text-[#FFAB2D]" />
-                      <AiFillStar className="text-[#FFAB2D]" />
-                      <AiFillStar className="text-[#FFAB2D]" />
-                      <AiFillStar className="text-[#FFAB2D]" />
-                    </div>
-                    <div className="flex items-center space-x-2 font-bold">
-                      {/* <p>{el.ratings}</p> */}
-                      <p>4.8</p>
-                      {/* <p>({el.totalReviews}) Reviews</p> */}
-                      <p>117 Reviews</p>
-                    </div>
-                  </div>
                   <div className="lg:basis-11/12">
                     <h2 className="font-bold capitalize">{el.title}</h2>
                     <p>{el.program_type}</p>
@@ -111,7 +100,7 @@ const TrackingSoftware = ({ softwareData }: any) => {
                   </Link>
                 </div>
               </div>
-            </Link>
+            </div>
           ))}
         </div>
       </div>
